936e79dc8c99b0c5aa4a6177c22361f3ed270aef
AlagesanRoja <163024815+AlagesanRoja18@users.noreply.github.com>
2024-04-26 02:50:38 -0400

    install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+wget http://ftp.de.debian.org/debian/pool/main/g/gtkglext/libgtkglext1_1.2.0-11_amd64.deb
+sudo dpkg -i libgtkglext1_1.2.0-11_amd64.deb
+sudo dpkg -i anydesk_6.3.2-1_amd64.deb
+anydesk
